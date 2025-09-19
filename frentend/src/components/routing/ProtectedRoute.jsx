533bd24c5edc07dc52969f783fcb2c65bdc52b75
akashkumarsky <akash220701@gmail.com>
2025-09-19 14:56:37 +0530

    changing protected route login to dashboard

--- frentend/src/components/routing/ProtectedRoute.jsx
+++ frentend/src/components/routing/ProtectedRoute.jsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 
+
 const ProtectedRoute = () => {
     const { token } = useContext(AuthContext);
 
     if (!token) {
         // Redirect to the /login page
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/dashboard" replace />;
     }
 
     return <Outlet />;
